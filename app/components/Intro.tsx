cb12afa9aa05b9ecf1b52bf86509659657cb0495
Azfar <azfarrazzaq23@gmail.com>
2025-04-22 19:53:32 +0500

    Created Intro Section

---
+++ app/components/Intro.tsx
@@ -0,0 +1,18 @@
+export default function Intro() {
+  return (
+    <div className="bg-introBg h-screen px-8 pt-32">
+      <div>
+        <div className="inline-flex flex-col items-start justify-center text-6xl text-darkBrown font-Bodoni font-bold">
+          <h1>KHALIL</h1>
+          <h1 className="text-goldFont w-full text-center">&</h1>
+          <h1>KHALIL</h1>
+        </div>
+        <p className="pt-4 text-lightBrown ">
+          Future-focused and dedicated to excellence, Akin is an elite law firm
+          that helps clients anticipate what is next and navigate a path to
+          success.
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import Intro from "~/components/Intro";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -8,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return null;
+  return <Intro />;
 }
