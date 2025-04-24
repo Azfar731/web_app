4603dcd7c6124e4bd7860b3c2cf8ba120ebedd6b
Azfar <azfarrazzaq23@gmail.com>
2025-04-25 01:21:40 +0500

    added styles for desktop in intro section

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,13 +1,13 @@
 export default function Intro() {
   return (
-    <div className="bg-introBg h-screen px-6 pt-32">
+    <div className="bg-introBg h-screen px-6 pt-32 lg:flex lg:items-center lg:justify-end lg:pr-32">
       <div>
         <div className="inline-flex flex-col items-start justify-center text-6xl text-darkBrown font-Bodoni font-bold">
           <h1>KHALIL</h1>
           <h1 className="text-goldFont w-full text-center">&</h1>
           <h1>KHALIL</h1>
         </div>
-        <p className="pt-4 text-lightBrown ">
+        <p className="pt-4 text-lightBrown lg:w-92 lg:text-xl">
           Future-focused and dedicated to excellence, Akin is an elite law firm
           that helps clients anticipate what is next and navigate a path to
           success.
