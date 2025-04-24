83a2fad0f470db4406e5994daa1da2cd3a4475f3
Azfar <azfarrazzaq23@gmail.com>
2025-04-25 03:21:36 +0500

    edited styles for responsiveness

--- app/components/Clients.tsx
+++ app/components/Clients.tsx
@@ -5,6 +5,11 @@ export default function Clients_section() {
     { src: "/assets/HBL-logo.png", alt: "HBL Logo" },
     { src: "/assets/Easypaisa-logo.png", alt: "Easy paisa logo" },
     { src: "/assets/meezan-bank-logo.png", alt: "Meezan bank logo" },
+    { src: "/assets/Easypaisa-logo.png", alt: "Easy paisa logo" },
+    { src: "/assets/HBL-logo.png", alt: "HBL Logo" },
+    { src: "/assets/meezan-bank-logo.png", alt: "Meezan bank logo" },
+    { src: "/assets/Easypaisa-logo.png", alt: "Easy paisa logo" },
+    { src: "/assets/HBL-logo.png", alt: "HBL Logo" },
   ];
 
   return (
@@ -15,7 +20,7 @@ export default function Clients_section() {
       <h2 className="text-gray-800 text-4xl uppercase font-Cinzel">
         Trusted By
       </h2>
-      <div className="grid grid-cols-2 gap-4 mt-8 px-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-6 xl:gap-14 mt-8 px-4">
         {clients.map((client) => (
           <Brand_Logos src={client.src} alt={client.alt} />
         ))}
--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,15 +1,20 @@
 export default function Contact_section() {
   return (
-    <div className=" relative py-12 px-12 h-102 sm:h-48 md:h-64 bg-[url('/images/landscape3.jpg')]  bg-cover bg-center text-white font-EBGaramond">
+    <div className=" relative py-12 px-12 h-102 sm:h-48 md:h-64 bg-[url('/images/landscape3.jpg')] md:bg-[url('/images/landscape2.jpg')]  bg-cover bg-center text-white font-EBGaramond">
       <div className="absolute inset-0 bg-black/70"></div>
 
-      <div className="relative z-10 h-full">
-        <div className="text-center text-4xl  uppercase font-Bodoni font-bold">
+      <div className="relative z-10 h-full md:flex md:items-center md:justify-around lg:justify-center lg:gap-32 ">
+        <div className="text-center text-4xl  uppercase font-Bodoni font-bold md:hidden">
           <h1>
             Khalil <br /> & <br /> Khalil
           </h1>
         </div>
-        <div className="pt-6 flex flex-col gap-6 justify-start">
+        <div className="text-center text-4xl  uppercase font-Bodoni font-bold max-md:hidden">
+          <h1>
+            Khalil & Khalil
+          </h1>
+        </div>
+        <div className="pt-6 md:pt-0 flex flex-col lg:flex-row lg:items-center gap-6 lg:gap-12 justify-start md:border-l md:border-white md:pl-6 lg:pl-12 ">
           <div id="address">
             <p>
               303 Peachtree Street NE
