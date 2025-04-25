9eaf300c263f0803f4909a8f5ca82184e6f68de6
Azfar <azfarrazzaq23@gmail.com>
2025-04-25 14:21:14 +0500

    Created members page

--- app/routes/members.tsx
+++ app/routes/members.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs } from "react-router";
 import type { Route } from "./+types/members";
 import membersData from "~/data/team.json";
+import { toTitleCase } from "~/utility/functions";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { name } = params;
@@ -22,21 +23,63 @@ export default function MemberPage({ loaderData }: Route.ComponentProps) {
   console.log("Member Data", memberData);
   return (
     <div className="px-6 py-20 pt-32 bg-white min-h-screen">
-      <div className="flex flex-col items-center justify-center gap-8 ">
+      <div className="pb-10 flex flex-col items-center justify-center gap-8 border-b border-black ">
         <img
           src={memberData.img}
           className="max-h-screen"
           alt={`${memberData.name}'s profile picture`}
         />
-        <div className="flex flex-col text-black">
-          <h2>{memberData.title}</h2>
-          <h1 className="text-2xl font-bold">{memberData.name}</h1>
-          {memberData.description.map((desc, index) => (
-            <p key={index} className="text-gray-600">
-              {desc}
-            </p>
-          ))}
+        <div className="flex flex-col gap-3 text-black">
+          <h2 className="text-gray-400 font-Cinzel">{memberData.title}</h2>
+          <h1 className="text-3xl  text-bronze font-Cinzel">
+            {toTitleCase(memberData.name)}
+          </h1>
+
+          {memberData.description.map((desc, index) => {
+            const firstLetter = desc.charAt(0).toUpperCase();
+            const restOfString = desc.slice(1).toLowerCase();
+            return (
+              <p key={index} className="text-gray-900 font-EBGaramond">
+                <span className={index === 0 ? `text-5xl text-bronze` : ""}>
+                  {firstLetter}
+                </span>
+                {restOfString}
+              </p>
+            );
+          })}
         </div>
+      </div>
+
+      <div className="border-b border-black pt-6 pb-10">
+        {memberData.info.map((info, index) => (
+          <div key={index} className="mt-4">
+            <h3 className="font-EBGaramond text-gray-700 font-bold mb-2">
+              {info.heading}
+            </h3>
+            <ul className="list-disc pl-5 ">
+              {info.points.map((point, i) => (
+                <li key={i} className="font-EBGaramond text-gray-600">
+                  {point}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div id="contact">
+        {
+            memberData.contact && (
+                <div className="py-10">
+                    <h3 className="font-EBGaramond text-gray-700 font-bold">Contact Information</h3>
+                    {Object.entries(memberData.contact).map(([key, value]) => (
+                        <div key={key} className="flex gap-2 mb-2">
+                            <span className="font-EBGaramond text-gray-700 capitalize">{key}:</span>
+                            <span className="font-EBGaramond text-gray-600">{value}</span>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
       </div>
     </div>
   );
