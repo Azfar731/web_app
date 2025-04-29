953ec4cb461522122f8acdb77a02beffb7ba13fe
Azfar <azfarrazzaq23@gmail.com>
2025-04-29 15:36:43 +0500

    added link for members page in desktop navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -85,7 +85,8 @@ export default function Navbar() {
               onMouseLeave={handleMouseLeave}
             >
               <div className="flex items-center hover:text-gray-300">
-                Team <RiArrowDropDownLine className="ml-1 w-5 h-5" />
+                <Link to={"members"}>Team</Link>{" "}
+                <RiArrowDropDownLine className="ml-1 w-5 h-5" />
               </div>
               <div
                 className={`
