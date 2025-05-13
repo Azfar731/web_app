8fed15aef4c281e6c34a0598245326aec5c13f85
Azfar <azfarrazzaq23@gmail.com>
2025-05-13 20:09:20 +0500

    removed grayscale functionality

--- app/components/Brand_logos.tsx
+++ app/components/Brand_logos.tsx
@@ -7,17 +7,13 @@ export default function Brand_Logos({
   src: string;
   alt: string;
 }) {
-  const [isColored, setIsColored] = useState(false);
   return (
-    <div
-      onClick={() => setIsColored((prev) => !prev)}
-      className=" bg-white  rounded-2xl shadow-md p-2 w-40 h-32 flex items-center justify-center"
-    >
+    <div className=" bg-white  rounded-2xl shadow-md p-2 w-40 h-32 flex items-center justify-center overflow-hidden">
       <img
         src={src}
         alt={alt}
-        className={`max-h-full max-w-full object-contain lg:grayscale lg:hover:grayscale-0  transition duration-200
-        ${isColored ? "max-lg:grayscale-0" : "max-lg:grayscale"} `}
+        className={`w-full h-full object-contain transition duration-200
+        `}
       />
     </div>
   );
